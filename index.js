5d69811da6030ede2c0998afe9525353e80e557d
Kozhin Vladimir <affka@affka.ru>
2016-11-05 11:30:47 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = require('./Connection.js');
+
+// AUTO-GENERATED Package files list
+if (!process.env.JII_NO_NAMESPACE) {
+    require('./Connection.js');
+    require('./QueryBuilder.js');
+    require('./Schema.js');
+}
