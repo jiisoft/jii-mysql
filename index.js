526547372a8e0191803029677a7cb8616b327dfd
Kozhin Vladimir <affka@affka.ru>
2016-12-25 23:01:15 +0700

    remove JII_NO_NAMESPACE env const

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 module.exports = require('./Connection.js');
-
-// AUTO-GENERATED Package files list
-if (!process.env.JII_NO_NAMESPACE) {
-    require('./Connection.js');
-    require('./QueryBuilder.js');
-    require('./Schema.js');
-}
